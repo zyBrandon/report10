23e4accf0dde952eb5116e4cb7d7591f700bdf2f
18779106195 <>
2018-12-18 11:25:05 +0800

    zy

--- Animate/ViewController.swift
+++ Animate/ViewController.swift
@@ -10,16 +10,32 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    @IBOutlet weak var myView: UIView!
+    
+    var timer:Timer?
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view, typically from a nib.
+        Timer.scheduledTimer(withTimeInterval: 1, repeats: true) { (mytimer) in
+            self.myView.center.x += 20
+        }
     }
 
-    override func didReceiveMemoryWarning() {
-        super.didReceiveMemoryWarning()
-        // Dispose of any resources that can be recreated.
+    
+    @IBOutlet weak var btn: UIButton!
+    
+    
+    @IBAction func click(_ sender: Any) {
+        timer?.invalidate()
+        UIView.animate(withDuration: 2, delay: 1, options: .curveEaseOut, animations: {
+            self.myView.backgroundColor = UIColor.green
+            self.myView.transform = CGAffineTransform(rotationAngle: CGFloat(Double.pi)).scaledBy(x: 0.5, y: 0.5)
+            self.myView.transform = CGAffineTransform.identity
+        }) { (finished) in
+            
+        }
     }
-
+    
 
 }
 
